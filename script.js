40cde19083d7e75cfe6c3ca1343b1fb7cc6e7627
Nisarg <nisargs2001@gmail.com>
2022-01-28 12:20:38 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+function myFunction1() {
+  document.getElementById("myDropdown1").classList.toggle("show");
+}
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+function myFunction3() {
+  document.getElementById("myDropdown3").classList.toggle("show");
+}
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+function myFunction5() {
+  document.getElementById("myDropdown5").classList.toggle("show");
+}
